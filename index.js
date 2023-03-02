b85693942eb74c53bd7d4f11ab61968424c4f377
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2023-03-02 17:47:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //toggling stream visibility, the streams won't load the the start if you don't enable them
-function toggle(element) {
+function toggle(elementId) {
+  var element = document.getElementById(elementId);
+  if (!element) {
+    console.log("Element not found: " + elementId);
+    return;
+  }
   if (element.style.display === "block") {
-    document.getElementById(element).style.display = 'none';
+    element.style.display = "none";
   } else if (element.style.display === "none") {
-    document.getElementById(element).style.display = 'block';
+    element.style.display = "block";
   } else {
-    document.createTextNode("Error while loading some of the streams (Error 0)");
+    var textNode = document.createTextNode(
+      "Error while loading some of the streams (Error 0)"
+    );
     document.body.appendChild(textNode);
   }
 }
