f06675ca0fc22046cb305bb97207196d14303a2a
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2023-03-03 16:10:21 -0500

    Test v2

--- index.js
+++ index.js
@@ -10,14 +10,4 @@ function toggleElement(elementId) {
         } else {
           element.style.display = "block";
         }
-        // reorganize the iframes using flexbox
-        var streams = document.querySelector(".streams");
-        streams.style.display = "flex";
-        streams.style.flexWrap = "wrap";
-        var streamContainers = document.querySelectorAll(".stream-container");
-        var numStreams = streamContainers.length;
-        for (var i = 0; i < numStreams; i++) {
-          streamContainers[i].style.width = "50%";
-          streamContainers[i].style.height = "50vh";
-        }
       }
