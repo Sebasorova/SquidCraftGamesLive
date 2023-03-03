98ad3b55081567b41e0ef15504c7b6cdc74c65d1
SebastianSoftware <56850980+SebastianSoftware@users.noreply.github.com>
2023-03-03 16:04:12 -0500

    Restore index.js

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ function toggleElement(elementId) {
     console.log("Element not found: " + elementId);
     return;
   }
-  if (element.style.display === "block") {
-    // Hide the element by removing it from the DOM
-    element.remove();
+  if (element.style.visibility === "hidden") {
+    element.style.visibility = "visible";
   } else {
-    // Show the element by adding it back to the DOM
-    var container = document.querySelector(".streams");
-    container.appendChild(element);
+    element.style.visibility = "hidden";
   }
   // reorganize the iframes using flexbox
   var streams = document.querySelector(".streams");
@@ -19,7 +16,7 @@ function toggleElement(elementId) {
   var streamContainers = document.querySelectorAll(".stream-container");
   var numStreams = streamContainers.length;
   for (var i = 0; i < numStreams; i++) {
-    streamContainers[i].style.width = "100%";
-    streamContainers[i].style.height = "100%";
+    streamContainers[i].style.width = "50%";
+    streamContainers[i].style.height = "50vh";
   }
 }
